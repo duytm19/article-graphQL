17191ced825748a2e89ace10c26cabd0f00a212a
duytm19 <tranminhduy1912@gmail.com>
2025-03-21 00:43:31 +0700

    set up apollo

---
+++ resolvers.ts
@@ -0,0 +1,14 @@
+
+import Article from "./models/article.model";
+
+export const resolvers={
+    Query:{
+        getListArticle:async()=>{
+            const articles = await Article.find({
+                deleted:false
+            })
+            return articles
+        }
+
+    }
+}
